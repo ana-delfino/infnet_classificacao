2243fad55ed970060f86a7e29c389a18b286bd81
Ana Paula Santos Delfino <57999613+ana-delfino@users.noreply.github.com>
2025-02-20 20:41:01 -0300

    add code

--- plot_aux.py
+++ plot_aux.py
@@ -1,6 +1,5 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.metrics import silhouette_samples, silhouette_score
 import matplotlib.cm as cm
 import pandas as pd
 import numpy as np
@@ -210,50 +209,3 @@ def plot_boxplots_by_columns_hue(df, column_names, hue):
     plt.tight_layout()
     plt.show()   
 
-def plot_silhouette(X, cluster_labels):
-    # cluster_labels = df_normalized['KMeans_Cluster']
-    silhouette_avg = silhouette_score(X, cluster_labels)
-    n_clusters = cluster_labels.nunique()
-    y_lower = 10
-    sample_silhouette_values = silhouette_samples(X, cluster_labels)
-    fig, (ax1, ax2) = plt.subplots(1, 2)
-    fig.set_size_inches(18, 7)
-
-    ax1.set_xlim([-0.1, 1])
-    # ax1.set_ylim([0, 1000])
-    ax1.set_ylim([0, len(X) + (n_clusters + 1) * 10])
-    clusters = cluster_labels.unique().tolist()
-    for i in clusters:
-        ith_cluster_silhouette_values = sample_silhouette_values[cluster_labels == i]
-        ith_cluster_silhouette_values.sort()
-
-        size_cluster_i = ith_cluster_silhouette_values.shape[0]
-        y_upper = y_lower + size_cluster_i
-
-        color = cm.nipy_spectral(float(i) / n_clusters)
-        ax1.fill_betweenx(np.arange(y_lower, y_upper), 0, ith_cluster_silhouette_values,
-                          facecolor=color, edgecolor=color, alpha=0.7)
-
-        ax1.text(-0.05, y_lower + 0.5 * size_cluster_i, str(i))
-        y_lower = y_upper + 10
-
-    ax1.set_title("The silhouette plot for the various clusters")
-    ax1.set_xlabel("The silhouette coefficient values")
-    ax1.set_ylabel("Cluster label")
-
-    ax1.axvline(x=silhouette_avg, color="red", linestyle="--")
-    ax1.set_yticks([])  
-    ax1.set_xticks([-0.1, 0, 0.2, 0.4, 0.6, 0.8, 1])
-
-    colors = cm.nipy_spectral(cluster_labels.astype(float) / n_clusters)
-    ax2.scatter(X.iloc[:, 5], X.iloc[:, 6], marker='.', s=100, lw=0, alpha=0.9,
-                c=colors, edgecolor='k')
-
-    ax2.set_title("The visualization of the clustered data.")
-    ax2.set_xlabel("Feature space for the 1st feature")
-    ax2.set_ylabel("Feature space for the 2nd feature")
-
-    plt.suptitle(("Silhouette analysis for Hierarchical clustering on sample data "
-                  "with n_clusters = %d" % n_clusters),
-                 fontsize=14, fontweight='bold')
-    plt.show()
